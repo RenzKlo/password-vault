4fcd83c292cf77f9a30a35963866cb119981a736
RenzKLo <kentlorenz.daria@wvsu.edu.ph>
2024-09-17 00:43:02 +0800

    refactored to add dotenv

--- db_connector.py
+++ db_connector.py
@@ -1,12 +1,18 @@
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
 
 class Database:
     _instance = None
 
     def __new__(cls):
         if cls._instance is None:
+            load_dotenv()  # Load environment variables from .env file
+            database_url = os.getenv('DATABASE_URL')
+            if not database_url:
+                raise ValueError("DATABASE_URL environment variable not set")
             cls._instance = super(Database, cls).__new__(cls)
-            cls._instance.client = MongoClient("mongodb://localhost:27017/")
+            cls._instance.client = MongoClient(database_url)
             cls._instance.db = cls._instance.client['auth_system_db']
         return cls._instance
 
@@ -17,11 +23,9 @@ class Database:
 def get_db():
     return Database().get_db()
 
-
 if __name__ == "__main__":
-    try :
+    try:
         db = get_db()
         print("Database instance created.")
     except Exception as e:
         print("Error: ", e)
-
--- key_manager.py
+++ key_manager.py
@@ -1,26 +1,28 @@
 import os
 from cryptography.fernet import Fernet
+from dotenv import load_dotenv, set_key
 
 class UserKeysManager:
     def __init__(self):
-        self.master_key_path = 'master.key'
-        self.master_key = self.load_master_key()
+        load_dotenv()  # Load environment variables from .env file
+        self.master_key = self.__load_master_key()
         self.master_cipher_suite = Fernet(self.master_key)
 
-    def load_master_key(self):
-        # Load or generate master key
-        if os.path.exists(self.master_key_path):
-            with open(self.master_key_path, 'rb') as key_file:
-                return key_file.read()
+    def __load_master_key(self):
+        # Load master key from environment variable or generate a new one
+        master_key = os.getenv('MASTER_KEY')
+        if master_key:
+            return master_key.encode()
         else:
             key = Fernet.generate_key()
-            with open(self.master_key_path, 'wb') as key_file:
-                key_file.write(key)
+            self.__save_master_key_to_env(key)
             return key
 
-    def generate_key(self):
-        # Generate a new Fernet key
-        return Fernet.generate_key()
+    def __save_master_key_to_env(self, key):
+        # Save the generated master key to the .env file
+        env_file = '.env'
+        set_key(env_file, 'MASTER_KEY', key.decode())
+
 
     def encrypt(self, data):
         # Encrypt data using master cipher suite
